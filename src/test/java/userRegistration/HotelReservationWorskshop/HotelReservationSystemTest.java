6d84572b735cc9d0efcbb28fb8ef6534e7f73a30
Your Name <you@example.com>
2021-09-22 22:29:38 +0530

    Added: Test case for cheaphotel for regular days

--- src/test/java/userRegistration/HotelReservationWorskshop/HotelReservationSystemTest.java
+++ src/test/java/userRegistration/HotelReservationWorskshop/HotelReservationSystemTest.java
@@ -2,21 +2,25 @@ package userRegistration.HotelReservationWorskshop;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+
+
 public class HotelReservationSystemTest {
+	HotelReservationSystem hotelreservation = new HotelReservationSystem();
+
+	@Before
+	public void HotelAdded() {
+		assertTrue(hotelreservation.listHotel("Lakewood", 110));
+		assertTrue(hotelreservation.listHotel("BridgeWood", 160));
+		assertTrue(hotelreservation.listHotel("RidgeWood", 220));
+		int hotelCount = hotelreservation.hotelCount();
+		assertEquals(3,hotelCount);
+	}
 
 	@Test
-	public void HotelAdded()
-	{
-		HotelReservationSystem hotelreservation = new HotelReservationSystem();
-		String name = "Lakewood";
-		double weekdayRegularPrice = 110;
-		double weekdayRewardPrice = 80;
-		double WeekendRegularRate = 90;
-		double WeekendRewardRate = 80;
-		
-		Boolean check = hotelreservation.listHotel(name, weekdayRegularPrice, weekdayRewardPrice, WeekendRegularRate, WeekendRewardRate);
-		assertEquals(true,check);
-		
-		
+	public void checkCheapHoteForRegalurCustomer() {
+		String cheapHotel = hotelreservation.cheapHotelForRegularCustomer("10Sep2020", "11Sep2020");
+		assertEquals("Lakewood", cheapHotel);
 	}
 }
